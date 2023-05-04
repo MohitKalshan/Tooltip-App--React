076b24829f62ff12a18c1c6f86b8e6df48e93094
MohitKalshan <mohitkalshan14.mk@gmail.com>
2023-05-04 22:46:07 +0530

    styling added

--- src/component/Tooltip.js
+++ src/component/Tooltip.js
@@ -1,18 +1,15 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
+
 export default function Tool_tip({position}){
+  const message =`Thanks for hovering! I'm a tooltip`;
   return (
-    <Box sx={{ width: 500 }}>
-      <Grid container justifyContent="center">
-        <Grid item>
-          <Tooltip title="Thank You" placement={!position ? "top" : position} arrow>
-            <Button className="btn">Hover On Me</Button>
+    <Box sx={{ width: 500 }} textAlign={"center"}>
+          <Tooltip sx={{ fontSize: 500 }} title={message} placement={!position ? "top" : position} arrow>
+            <Button className="btn">Hover Over Me</Button>
           </Tooltip>
-        </Grid>
-      </Grid>
     </Box>
   );
 }
